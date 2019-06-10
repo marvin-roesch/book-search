7049ea2c9eb8a14ed4d6c47ed4bd4e62c941ec2f
Marvin Rösch <marvinroesch99@gmail.com>
2019-06-10 20:08:54 +0200

    Initial commit

---
+++ backend/build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    application
+    kotlin("jvm") version "1.3.21"
+    id("com.github.johnrengelman.shadow") version "5.0.0"
+}
+
+group = "io.paleocrafter"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    jcenter()
+    maven {
+        url = uri("https://github.com/psiegman/mvn-repo/raw/master/releases")
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.ktor:ktor-server-core:1.2.1")
+    implementation("io.ktor:ktor-server-netty:1.2.1")
+    implementation("org.jetbrains.exposed:exposed:0.14.2")
+    implementation("org.elasticsearch.client:elasticsearch-rest-high-level-client:7.1.1")
+    implementation("nl.siegmann.epublib:epublib-core:3.1")
+
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "io.ktor.server.netty.EngineMain"
+}
+
+tasks.withType<Jar> {
+    archiveFileName.set("book-search.jar")
+    manifest {
+        attributes(
+            mapOf(
+                "Main-Class" to application.mainClassName
+            )
+        )
+    }
+}
---
+++ backend/src/main/kotlin/io/paleocrafter/booksearch/Application.kt
@@ -0,0 +1,27 @@
+package io.paleocrafter.booksearch
+
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(ConditionalHeaders)
+    install(Compression)
+    install(StatusPages) {
+        exception<NotImplementedError> { call.respond(HttpStatusCode.NotImplemented) }
+    }
+
+    routing {
+
+    }
+}
+
+fun main() {
+    embeddedServer(Netty, 8080, module = Application::main).start()
+}
